9e1882176e8bb299305a99cae8e3efd689289e29
Radi Rusadi <radi.rusadi.s@gmail.com>
2021-05-07 20:12:22 +0700

    Ganerate data recent transactions for chart widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:expensive_track/models/transaction.dart';
+import 'package:expensive_track/widgets/chart.dart';
 import 'package:expensive_track/widgets/new_transaction.dart';
 import 'package:expensive_track/widgets/transaction_list.dart';
 import 'package:flutter/material.dart';
@@ -66,6 +67,18 @@ class _MyHomePageState extends State<MyHomePage> {
     // ),
   ];
 
+  List<Transaction> get _recentTransactions {
+    return _userTransactions
+        .where(
+          (element) => element.date.isAfter(
+            DateTime.now().subtract(
+              Duration(days: 7),
+            ),
+          ),
+        )
+        .toList();
+  }
+
   void _addNewTransaction(String title, double amount) {
     final newTransaction = Transaction(
       id: DateTime.now().toString(),
@@ -101,14 +114,7 @@ class _MyHomePageState extends State<MyHomePage> {
       body: SingleChildScrollView(
         child: Column(
           children: [
-            Container(
-              width: double.infinity,
-              child: Card(
-                color: Colors.blue,
-                elevation: 5,
-                child: Text('Chart'),
-              ),
-            ),
+            Chart(_recentTransactions),
             TransactionList(_userTransactions),
           ],
         ),
