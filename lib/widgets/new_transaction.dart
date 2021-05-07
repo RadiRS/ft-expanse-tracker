9f70609235d606e2bf8f75821fbcfb6fc1085e86
Radi Rusadi <radi.rusadi.s@gmail.com>
2021-05-07 14:37:25 +0700

    Added simple validation check at submit & text transform

--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class NewTransaction extends StatelessWidget {
   final Function addTransaction;
@@ -6,6 +7,18 @@ class NewTransaction extends StatelessWidget {
   final amountController = TextEditingController();
 
   NewTransaction(this.addTransaction);
+
+  void onSubmit() {
+    final enteredTitle = titleController.text;
+    final enteredAmount = double.parse(amountController.text);
+
+    if (enteredTitle.isEmpty || enteredAmount <= 0) return;
+
+    addTransaction(
+      titleController.text,
+      double.parse(amountController.text),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -16,22 +29,17 @@ class NewTransaction extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.end,
           children: [
             TextField(
-              decoration: InputDecoration(
-                labelText: 'Title',
-              ),
               controller: titleController,
+              decoration: InputDecoration(labelText: 'Title'),
             ),
             TextField(
-              decoration: InputDecoration(
-                labelText: 'Amount',
-              ),
               controller: amountController,
+              decoration: InputDecoration(labelText: 'Amount'),
+              keyboardType: TextInputType.numberWithOptions(decimal: true),
+              onSubmitted: (_) => onSubmit(),
             ),
             TextButton(
-              onPressed: () {
-                addTransaction(
-                    titleController.text, double.parse(amountController.text));
-              },
+              onPressed: onSubmit,
               child: Text('Add Transaction'),
             ),
           ],
